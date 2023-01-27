d7169eef3c059831a7e17f9733bf2124da5da94e
Ethan Storm <stormethan10@gmail.com>
2023-01-27 11:04:03 -0800

    just stuff

--- classes.js
+++ classes.js
@@ -1,6 +1,6 @@
 // creating classes and using them based on what I learned from reading in a day
 
-class Animal {
+/* class Animal {
 	constructor(species) {
 		this.species = species
 	}
@@ -15,15 +15,19 @@ class Dog extends Animal {
 		super('Dog')
 		this.name = name
 		this.age = age
+		this.alive = true
+		const sound = 'woof'
 	}
 
 	bark() {
-		console.log(`${this.name} barks!`)
+		console.log(`${this.name} barks!`, this.sound)
 	}
 }
 
-const Johnny = new Dog('johnny', 2)
+//console.log(sound)
 
+const Johnny = new Dog('johnny', 2)
+console.log(Johnny.alive)
 Johnny.breath()
 Johnny.bark()
 
@@ -40,3 +44,20 @@ const ANIMALS = []
 ANIMALS.push(Johnny)
 
 sessionStorage.setItem('ANIMALS', ANIMALS)
+*/
+
+let dog = {
+	breed: 'Beagle',
+	weight: 30,
+	group: 'Hound',
+}
+let key = 'weight'
+
+console.log(dog[key] === 30)
+console.log(dog.hasOwnProperty('weight'))
+console.log('weight' in dog)
+console.log(dog['weight'] == '30')
+console.log(dog.weight ? true : false)
+console.log(dog.key === 30)
+
+// access object properties with spaces like this object["hello world"] >> "greetings"
